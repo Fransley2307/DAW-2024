5bb7a1c10233394afecf47e0740ae43d4de86443
Fransley Brustolim Ferreira <fransley0@gmail.com>
2024-11-13 22:56:13 -0300

    Aula 13/11/2024

---
+++ introducao-js/type-boolean.js
@@ -0,0 +1,22 @@
+const value1 = true;
+const value2 = false;
+
+console.log(value1 == value2);
+
+//Thrusty / Falsy --> Variaveis vazias ou com valor 0, se submeter 
+//a um teste logico, se tornaram um tipo boolean(Thursty/falsy)
+
+//Teste Logico 1
+if (value1){
+    console.log('verdadeiro')
+}else{
+    console.log('falso')
+}
+
+//Teste Logico 
+if (value1){
+    console.log('verdadeiro')
+}
+
+//O de cima vira:
+if (value1) console.log('verdadeiro')
---
+++ introducao-js/type-string.js
@@ -0,0 +1,10 @@
+/*const Constante, valor dela não muda depois do inicial 
+var Pode alterar o valor da variavel, fica global 
+let Pode alterar o valor da variavel, fica dentro do escopo que foi criado
+*/
+
+const primeiroTexto = 'Olá';
+const segundoTexto = 'Mundo';
+
+console.log(primeiroTexto + ' '+ segundoTexto);
+console.log(``)
